5b6da9dca88b79b97b7a40b513c04418120c3919
duogbachdev <contact.duogbachdev@gmail.com>
2023-11-24 22:39:20 +0700

    Authentication v1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import usersRouter from '~/routes/users.routes'
+import databaseService from '~/services/database.services'
 
 const app = express()
 const port = 3000
@@ -7,6 +8,6 @@ const port = 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
+databaseService.connect()
 app.use(express.json())
 app.use('/users', usersRouter)
